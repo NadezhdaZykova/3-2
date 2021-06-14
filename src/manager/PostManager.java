270a785ac64f5e6419b7d40046fc944f386775d6
Nadya Zykova <xilda777@gmail.com>
2021-06-14 21:55:26 +0400

    add manager

---
+++ src/manager/PostManager.java
@@ -0,0 +1,21 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search() {
+        String query;
+        int count;
+
+        return null;
+    }
+
+    public void delete()
+
+    {
+        int postId;
+
+    }
+}
