1c968dfb45cc2ff72e0a1d75561b9f16ab897248
Nadya Zykova <xilda777@gmail.com>
2021-06-16 21:12:24 +0400

    Manager

--- src/manager/PostManager.java
+++ src/manager/PostManager.java
@@ -4,15 +4,21 @@ import domain.Post;
 
 public class PostManager {
     private Post[] posts;
+    int ownerID;
+    String domain;
+    String query;
+    boolean ownersOnly;
+    int count;
+    int offset;
+    int postId;
 
-    public Post[] search() {
-        String query;
-        int count;
+    public Post[] search(int count, Post[] posts ) {
+        Post[] posts = postManager.getPostsForFeed();
 
         return null;
     }
 
-    public void delete()
+    public void delete(int ownerID, int post_id)
 
     {
         int postId;
